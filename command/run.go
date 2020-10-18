05fbc2536cf200b87fee0d14bd53a460c33b0b24
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-19 03:13:15 +0700

    [ADD] command run app

---
+++ command/run.go
@@ -0,0 +1,25 @@
+package command
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber"
+)
+
+// Run function
+/**
+* Running the app
+* address: string for address listening app
+*          example: "localhost:3000" or ":3000"
+**/
+func Run(address string) {
+	var app *fiber.App
+
+	app = fiber.New()
+
+	if app != nil {
+		app.Listen(address)
+	} else {
+		log.Fatal("Error.")
+	}
+}
