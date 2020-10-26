26e8cb476f51b124dd9a06343afeb27342b13751
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-26 19:48:45 +0700

    [ADD] postgres support

--- database/connection/connect.go
+++ database/connection/connect.go
@@ -2,8 +2,10 @@ package connection
 
 import (
 	"log"
+	"os"
 
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
@@ -14,8 +16,19 @@ import (
 **/
 func Connect() bool {
 	var config *Config = getConfig()
-	var dsn string = getDsn(config)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	var dbms string = os.Getenv("DBMS")
+	var dsn string = getDsn(config, dbms)
+	var err error
+	var db *gorm.DB
+
+	if dbms == "postgres" {
+		db, err = gorm.Open(postgres.New(postgres.Config{
+			DSN:                  dsn,
+			PreferSimpleProtocol: true,
+		}), &gorm.Config{})
+	} else {
+		db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	}
 
 	if err != nil {
 		log.Fatal(err)
--- database/connection/dsn.go
+++ database/connection/dsn.go
@@ -7,7 +7,26 @@ import (
 /**
 * get dsn for connect to database
 **/
-func getDsn(config *Config) string {
+func getDsn(config *Config, dbms string) string {
+	if dbms == "postgres" {
+		return getDsnPosgres(config)
+	}
+
+	return getDsnMysql(config)
+}
+
+/**
+* get dsn for connect to database mysql
+**/
+func getDsnMysql(config *Config) string {
 	format := "%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=True&loc=Local"
 	return fmt.Sprintf(format, config.User, config.Password, config.Host, config.Port, config.Dbname, config.Charset)
+}
+
+/**
+* get dsn for connect to database postgresql
+**/
+func getDsnPosgres(config *Config) string {
+	format := "host=%s port=%s user=%s password=%s dbname=%s sslmode=disable TimeZone=%s"
+	return fmt.Sprintf(format, config.Host, config.Port, config.User, config.Password, config.Dbname, config.Timezone)
 }
