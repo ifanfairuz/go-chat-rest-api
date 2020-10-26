35f11b802a7ab05601c90df4b8416d5ce2f4a463
Ifan Fairuz <ifanfairuz@gmail.com>
2020-10-26 18:25:26 +0700

    [CHG] all except me

--- handler/users/all.go
+++ handler/users/all.go
@@ -3,13 +3,15 @@ package users
 import (
 	"github.com/gofiber/fiber/v2"
 
+	Models "github.com/ifanfairuz/go-chat-rest-api/database/models"
 	Users "github.com/ifanfairuz/go-chat-rest-api/database/query/users/get"
 	Network "github.com/ifanfairuz/go-chat-rest-api/lib/network"
 )
 
 // All handler
 func All(c *fiber.Ctx) error {
-	users, err := Users.All()
+	token := c.Locals("token").(Models.Token)
+	users, err := Users.AllExcept(token.Email)
 
 	if err != nil {
 		return c.Status(400).JSON(&Network.ErrResponse{
